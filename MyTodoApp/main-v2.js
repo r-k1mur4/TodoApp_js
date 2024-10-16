9b873539d561041d768016e6dfd460b21f460a49
Ryuki Kimura <ryuki.kimura@gaxi.co.jp>
2024-10-16 16:40:08 +0900

    ローカルストレージの作成

---
+++ MyTodoApp/main-v2.js
@@ -0,0 +1,64 @@
+'use strict';
+
+{
+  let todos;
+
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+
+  const renderTodo = (todo) => {
+    /*
+    - li
+      - label
+        - input
+        - span
+      - button
+    */
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    input.checked = todo.isCompleted;
+    const span = document.createElement('span');
+    span.textContent = todo.title;
+    const label = document.createElement('label');
+    label.appendChild(input);
+    label.appendChild(span);
+    const button = document.createElement('button');
+    button.textContent = 'x';
+    button.addEventListener('click', () => {
+      // if (confirm('Sure?') === false) {
+      if (!confirm('Sure?')) {
+          return;
+      }
+      li.remove();
+    });
+    const li = document.createElement('li');
+    li.appendChild(label);
+    li.appendChild(button);
+    document.querySelector('#todos').appendChild(li);
+  };
+
+  const renderTodos = () => {
+    todos.forEach((todo) => {
+      renderTodo(todo);
+    });
+  };
+
+  document.querySelector('#add-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector('#add-form input');
+    const todo = {
+      title: input.value,
+      isCompleted: false,
+    };
+    renderTodo(todo);
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    input.value = '';
+    input.focus();
+  });
+
+  renderTodos();
+}
--- MyTodoApp/main.js
+++ MyTodoApp/main.js
@@ -1,64 +1,82 @@
-'use strict';
+"use strict";
 
 {
-  const todos = [
-    {title: 'aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa', isCompleted: false},
-    {title: 'bbb', isCompleted: true},
-    {title: 'ccc', isCompleted: false},
-    {title: 'ddd', isCompleted: false},
-  ];
+
+  // let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
 
- const renderTodo = (todo) => {
-  /*
+  let todos;
+
+// localStorageにtodosプロパティがあるか確認
+if (localStorage.getItem('todos') === null) {
+  // ない場合はからの配列を作成
+  todos = [];
+} else {
+  // ある場合はtodosを読み込み
+  todos = JSON.parse(localStorage.getItem('todos'));
+}
+
+  const renderTodo = (todo) => {
+    /*
     - li
       - label
         - input
         - span
       - button
-  */
+    */
 
-  const input = document.createElement('input');
-  input.type = 'checkbox';
-  input.checked = todo.isCompleted;
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.checked = todo.isCompleted;
 
-  const span = document.createElement('span');
-  span.textContent = todo.title;
+    const span = document.createElement("span");
+    span.textContent = todo.title;
 
-  const label = document.createElement('label');
+    const label = document.createElement("label");
 
-  label.appendChild(input);
-  label.appendChild(span);
+    label.appendChild(input);
+    label.appendChild(span);
 
-  const button = document.createElement('button');
-  button.textContent = 'x';
+    const button = document.createElement("button");
+    button.textContent = "x";
+    // 削除ボタン
+    button.addEventListener('click', () => {
+      if (!confirm('Sure?')) {
+        return;
+      }
+        li.remove();
+    });
 
-  const li = document.createElement('li');
-  li.appendChild(label);
-  li.appendChild(button);
 
-  document.querySelector('#todos').appendChild(li);
-
- };
+    const li = document.createElement("li");
+    li.appendChild(label);
+    li.appendChild(button);
+    // ulタグにliを追加していく
+    document.querySelector("#todos").appendChild(li);
+  };
 
   const renderTodos = () => {
-    todos.forEach((listItem) => {
-      renderTodo(listItem);
+    todos.forEach((todo) => {
+      renderTodo(todo);
     });
   };
 
   // 項目の追加
-  document.querySelector('#add-form').addEventListener('submit', (e) => {
+  document.querySelector("#add-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    const input = document.querySelector('#add-form input');
-    const  listItem = {
+    const input = document.querySelector("#add-form input");
+    const todo = {
       title: input.value,
       isCompleted: false,
-    }
-    renderTodo(listItem);
-    input.value = '';
+    };
+    renderTodo(todo);
+    todos.push(todo);
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    input.value = "";
     input.focus();
-  })
+  });
 
   renderTodos();
 
